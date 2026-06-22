382c42ac2e615e63d1a87b0b9eb8cc97c2bb56a0
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-22 20:53:09 +0530

    Update main.tf

--- infra/20-rds/main.tf
+++ infra/20-rds/main.tf
@@ -5,7 +5,7 @@ module "rds" {
   identifier              = local.identifier
   availability_zone       = local.availability_zone        # var.availability_zone
   engine                  = "mysql"
-  engine_version          = "8.0"
+  engine_version          = "8.0.40"
   instance_class          = "db.t3.micro"
   allocated_storage       = 10  # 20
   storage_type            = "gp2"
