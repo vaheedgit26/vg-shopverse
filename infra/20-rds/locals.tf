6eed3dfc40549ae44fde06c4f2a6a87c53aa2109
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-22 20:55:21 +0530

    Update locals.tf

--- infra/20-rds/locals.tf
+++ infra/20-rds/locals.tf
@@ -2,9 +2,9 @@ locals {
   resource_name = "${var.project}/${var.env}"
  
   availability_zone      = data.terraform_remote_state.vpc.outputs.availability_zones[0]
-  sg_name                = "${local.resource_name}-postgre-rds-sg"
-  identifier             = "${local.resource_name}-postgre"
-  db_subnet_group_name   = "${local.resource_name}-postgre-rds-db-subnet-group"
+  sg_name                = "${local.resource_name}-mysql-rds-sg"
+  identifier             = "${local.resource_name}-mysql"
+  db_subnet_group_name   = "${local.resource_name}-mysql-rds-db-subnet-group"
   ecommerce_secret_json  = jsondecode(data.aws_secretsmanager_secret_version.ecommerce_secret_value.secret_string)
   vpc_security_group_ids = [data.terraform_remote_state.eks.outputs.cluster_security_group_id, data.terraform_remote_state.bastion.outputs.bastion_sg_id]
 
